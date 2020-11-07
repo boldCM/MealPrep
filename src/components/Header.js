8b65e806e0b0587154f7328b439c2094be5ab754
boldCM <carolin@cc-meier.de>
2020-11-07 12:07:56 +0100

    Add Styling to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,12 @@ import React from 'react';
 import styled from 'styled-components/macro';
 
 const Header = styled.h1`
+  display: flex;
   background-color: limegreen;
-  text-align: center;
+  align-items: center;
+  justify-content: center;
   margin: 0;
+  height: 100px;
 `;
 
 export default function HeaderComponent() {
